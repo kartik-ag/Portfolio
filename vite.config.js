666010295340b56b0efdbe767ae4dc5b95e553f8
Kartik Agarwal <agarwalkartik2018@gmail.com>
2024-08-22 23:36:24 +0530

    vite config changed

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0', // Binds the server to all network interfaces
+    port: 5173,      // Specify the port you want the app to run on
+  },
 })
